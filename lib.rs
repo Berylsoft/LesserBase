91008b425fec855cf33a20ef4885a1be2cc48fc4
stackinspector <stackinspector@126.com>
2022-06-11 09:06:01 +0800

    bson commit

--- lib.rs
+++ lib.rs
@@ -1,16 +1,18 @@
 #![allow(dead_code)]
 
 const VERSION: &str = "0.1-alpha";
-const EMPTY_HASH: [u8; HASH_LEN] = [0u8; HASH_LEN];
+type HashInner = [u8; HASH_LEN];
+const EMPTY_HASH: HashInner = [0u8; HASH_LEN];
 const HASH_LEN_I64: i64 = HASH_LEN as i64;
 
 use std::{path::{Path, PathBuf}, fs::{self, OpenOptions}, io::{self, Read, Write, Seek}};
 use serde::{Serialize, Deserialize};
+// use serde_with::serde_as;
 use blake3::{Hash, OUT_LEN as HASH_LEN, hash as hash_all};
 
 // region: util
 
-pub fn now() -> u64 {
+fn now() -> u64 {
     use std::time::{SystemTime, UNIX_EPOCH};
     SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis().try_into().unwrap()
 }
@@ -116,6 +118,7 @@ impl Repo {
         let commit = self.add_commit(c)?;
         self.update_ref("main", commit)?;
         println!("{:?}", self.get_ref("main"));
+        println!("{:?}", self.get_commit(commit));
         Ok(())
     }
 
@@ -148,17 +151,23 @@ impl Repo {
         fs::read(self.path.object(hash))
     }
 
-    fn add_commit(&self, commit: Commit) -> io::Result<Hash> {
-        let encoded = commit.encode();
-        let blob = encoded.as_bytes();
-        let hash = hash_all(blob);
+    fn add_commit(&self, commit: Commit) -> anyhow::Result<Hash> {
+        let blob = bson::to_vec(&CommitDocument::from(commit))?;
+        let hash = hash_all(&blob);
         // TODO: err: hash collision
         let mut file = OpenOptions::new().create_new(true).write(true).open(self.path.commit(hash))?;
-        file.write_all(blob)?;
+        file.write_all(&blob)?;
         Ok(hash)
+    }
+
+    fn get_commit(&self, hash: Hash) -> anyhow::Result<Commit> {
+        let file = OpenOptions::new().read(true).open(self.path.commit(hash))?;
+        let doc: CommitDocument = bson::from_reader(file)?;
+        Ok(Commit::from(doc))
     }
 }
 
+#[derive(Debug)]
 struct Commit {
     pub prev: Hash,
     pub ts: u64,
@@ -166,56 +175,95 @@ struct Commit {
     pub rev: Vec<Rev>,
 }
 
-impl Commit {
-    fn encode(&self) -> String {
-        let mut str = String::new();
-        str.push('*');
-        str.push(' ');
-        str.push_str(self.prev.to_hex().as_str());
-        str.push(' ');
-        str.push_str(&self.ts.to_string());
-        str.push(' ');
-        str.push_str(&self.author);
-        str.push('\n');
-        for r in self.rev.iter() {
-            str.push(r.kind.as_symbol());
-            str.push(' ');
-            str.push_str(r.hash.to_hex().as_str());
-            str.push(' ');
-            str.push_str(&r.path);
-            str.push('\n');
-        }
-        str
-    }
-}
-
+#[derive(Debug)]
 struct Rev {
     pub kind: RevKind,
     pub hash: Hash,
     pub path: String,
 }
 
+#[derive(Debug)]
 enum RevKind {
     Update,
     Remove,
 }
 
-impl RevKind {
-    fn as_symbol(&self) -> char {
-        match self {
-            RevKind::Update => '+',
-            RevKind::Remove => '-',
+// region: serde helper
+
+#[derive(Serialize, Deserialize)]
+struct CommitDocument {
+    pub prev: bson::Binary,
+    pub ts: u64,
+    pub author: String,
+    pub rev: Vec<RevDocument>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct RevDocument {
+    pub kind: u8,
+    pub hash: bson::Binary,
+    pub path: String,
+}
+
+fn hash_to_bson_bin(hash: Hash) -> bson::Binary {
+    bson::Binary { subtype: bson::spec::BinarySubtype::Generic, bytes: hash.as_bytes().to_vec() }
+}
+
+fn bson_bin_to_hash(raw: bson::Binary) -> Hash {
+    let inner: HashInner = raw.bytes.try_into().unwrap();
+    Hash::from(inner)
+}
+
+impl From<Commit> for CommitDocument {
+    fn from(commit: Commit) -> CommitDocument {
+        CommitDocument {
+            prev: hash_to_bson_bin(commit.prev),
+            ts: commit.ts,
+            author: commit.author,
+            rev: commit.rev.into_iter().map(|r| RevDocument {
+                kind: r.kind.into(),
+                hash: hash_to_bson_bin(r.hash),
+                path: r.path,
+            }).collect(),
         }
     }
+}
 
-    fn to_symbol(c: char) -> RevKind {
-        match c {
-            '+' => RevKind::Update,
-            '-' => RevKind::Remove,
+impl From<CommitDocument> for Commit {
+    fn from(doc: CommitDocument) -> Commit {
+        Commit {
+            prev: bson_bin_to_hash(doc.prev),
+            ts: doc.ts,
+            author: doc.author,
+            rev: doc.rev.into_iter().map(|r| Rev {
+                kind: r.kind.into(),
+                hash: bson_bin_to_hash(r.hash),
+                path: r.path,
+            }).collect(),
+        }
+    }
+}
+
+impl From<u8> for RevKind {
+    fn from(digit: u8) -> Self {
+        match digit {
+            0 => RevKind::Update,
+            1 => RevKind::Remove,
             _ => unreachable!(),
         }
     }
 }
+
+impl From<RevKind> for u8 {
+    fn from(kind: RevKind) -> Self {
+        match kind {
+            RevKind::Update => 0,
+            RevKind::Remove => 1,
+        }
+    }
+}
+
+// endregion
 
 #[derive(Debug)]
 pub enum Command {}
